bcd948f13457b922e5c748a64dd7788dd4a28ae8
myoungminjun <myoungminjun@gmail.com>
2025-02-12 23:27:27 +0900

    sixth commit user login and logout AND Session function add

---
+++ src/main/java/com/example/springscheduledevelop/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.example.springscheduledevelop.controller;
+
+public class LoginController {
+}
---
+++ src/main/java/com/example/springscheduledevelop/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.springscheduledevelop.dto;
+
+public class LoginRequestDto {
+}
